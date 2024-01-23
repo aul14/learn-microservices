f09f9b6f25d28fdcb15454aba6191df92087fc7e
aul14 <rahmanaulia336@gmail.com>
2024-01-23 15:31:00 +0700

    create api delete image media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
 
 const { Media } = require('../models');
 
@@ -18,7 +19,6 @@ router.get('/', async (req, res) => {
 
   return res.json({
     status: 'success',
-    message: 'data found',
     data: mappedMedia
   })
 })
@@ -59,6 +59,35 @@ router.post('/', (req, res) => {
 
   })
 
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'image media not found!'
+    })
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({
+        status: 'error',
+        message: err.message
+      })
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image deleted successfully'
+    })
+  });
 })
 
 module.exports = router;
