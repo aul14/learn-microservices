7a80425480146152fb55d1d41839ef60f6bd81e7
aul14 <rahmanaulia336@gmail.com>
2024-01-25 10:50:02 +0700

    integrasi api register to api-gateway

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,11 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usersHandler = require('./handler/users')
 
-const { APP_NAME } = process.env;
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('users');
-});
+router.post('/register', usersHandler.register);
 
 module.exports = router;
