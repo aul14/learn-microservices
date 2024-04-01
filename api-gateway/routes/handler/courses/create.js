cfedbd353b1e4cfaf6f74e1b42911667bcd9d291
aul14 <rahmanaulia336@gmail.com>
2024-04-01 15:19:30 +0700

    integrasi api gateway-service course (api courses)

---
+++ api-gateway/routes/handler/courses/create.js
@@ -0,0 +1,33 @@
+const apiAdapter = require('../../apiAdapter');
+
+const {
+    URL_SERVICE_COURSE
+} = process.env;
+
+const api = apiAdapter(URL_SERVICE_COURSE);
+
+module.exports = async (req, res) => {
+    try {
+        const course = await api.post('./api/courses', req.body);
+        return res.json(course.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({
+                status: 'error',
+                message: 'service unavailable!'
+            })
+        }
+
+        if (error.response) {
+            // If error.response exists, destructure properties from it
+            const { status, data } = error.response;
+            return res.status(status).json(data);
+        } else {
+            // If error.response is undefined, handle the error accordingly
+            return res.status(500).json({
+                status: 'error',
+                message: 'An unexpected error occurred.'
+            });
+        }
+    }
+}
