74fc1d5bdbca92628e1db3d36b010004673a74f3
aul14 <rahmanaulia336@gmail.com>
2024-04-02 13:20:04 +0700

    integrasi api gateway-service course (api myCourses)

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -16,6 +16,7 @@ const lessonsRouter = require('./routes/lessons');
 const imageCoursesRouter = require('./routes/imageCourses');
 const refreshTokensRouter = require('./routes/refreshTokens');
 const mentorsRouter = require('./routes/mentors');
+const myCoursesRouter = require('./routes/myCourses');
 
 const verifyToken = require('./middlewares/verifyToken');
 
@@ -37,6 +38,7 @@ app.use('/orders', ordersRouter);
 app.use('/image-courses', verifyToken, imageCoursesRouter);
 app.use('/payments', paymentsRouter);
 app.use('/refresh-tokens', refreshTokensRouter);
+app.use('/my-courses', verifyToken, myCoursesRouter);
 app.use('/mentors', verifyToken, mentorsRouter);
 
 module.exports = app;
