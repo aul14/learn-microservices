bad67061cff6b480c8f69ec64b2014987b9cf626
aul14 <rahmanaulia336@gmail.com>
2024-04-02 16:06:35 +0700

    create migrate table order and payment logs

--- service-order-payment/database/migrations/2024_04_02_155859_create_orders_table.php
+++ service-order-payment/database/migrations/2024_04_02_155859_create_orders_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('status')->default('pending');
+            $table->integer('user_id');
+            $table->integer('course_id');
+            $table->string('snap_url')->nullable();
+            $table->json('metadata')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
