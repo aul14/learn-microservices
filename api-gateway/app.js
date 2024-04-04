5a77cc96fecab969c178745889e875f97f87cb64
aul14 <rahmanaulia336@gmail.com>
2024-04-04 16:17:04 +0700

    create role & permission api gateway

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -20,6 +20,7 @@ const reviewsRouter = require('./routes/reviews');
 const webhookRouter = require('./routes/webhook');
 
 const verifyToken = require('./middlewares/verifyToken');
+const havePermission = require('./middlewares/permission');
 
 const app = express();
 
@@ -32,15 +33,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courses', coursesRouter);
-app.use('/chapters', verifyToken, chaptersRouter);
-app.use('/media', mediaRouter);
-app.use('/lessons', verifyToken, lessonsRouter);
-app.use('/orders', verifyToken, orderPaymentsRouter);
-app.use('/image-courses', verifyToken, imageCoursesRouter);
+app.use('/chapters', verifyToken, havePermission('admin'), chaptersRouter);
+app.use('/media', verifyToken, havePermission('admin', 'student'), mediaRouter);
+app.use('/lessons', verifyToken, havePermission('admin'), lessonsRouter);
+app.use('/orders', verifyToken, havePermission('admin', 'student'), orderPaymentsRouter);
+app.use('/image-courses', verifyToken, havePermission('admin'), imageCoursesRouter);
 app.use('/refresh-tokens', refreshTokensRouter);
-app.use('/my-courses', verifyToken, myCoursesRouter);
-app.use('/mentors', verifyToken, mentorsRouter);
-app.use('/reviews', verifyToken, reviewsRouter);
+app.use('/my-courses', verifyToken, havePermission('admin', 'student'), myCoursesRouter);
+app.use('/mentors', verifyToken, havePermission('admin'), mentorsRouter);
+app.use('/reviews', verifyToken, havePermission('admin', 'student'), reviewsRouter);
 app.use('/webhook', webhookRouter);
 
 module.exports = app;
--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const coursesHandler = require('./handler/courses');
 
-const verifyToken = require('../middlewares/verifyToken')
+const verifyToken = require('../middlewares/verifyToken');
+const havePermission = require('../middlewares/permission');
 
 router.get('/', coursesHandler.getAll);
 router.get('/:id', coursesHandler.get);
 
-router.post('/', verifyToken, coursesHandler.create);
-router.put('/:id', verifyToken, coursesHandler.update);
-router.delete('/:id', verifyToken, coursesHandler.destroy);
+router.post('/', verifyToken, havePermission('admin'), coursesHandler.create);
+router.put('/:id', verifyToken, havePermission('admin'), coursesHandler.update);
+router.delete('/:id', verifyToken, havePermission('admin'), coursesHandler.destroy);
 
 module.exports = router;
