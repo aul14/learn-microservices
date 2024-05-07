ae818f170d9faae4c5e5051717c673c381f1a10c
aul14 <rahmanaulia336@gmail.com>
2024-05-07 16:21:41 +0700

    set up front page v1

---
+++ bwamicro/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>BWAMICRO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Saya halaman utama
+      </main>
+    </div>
+  )
+}
